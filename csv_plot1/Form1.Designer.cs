0520b504ed7c1ab244912a88dfa932c09e7e31d8
huanjie1 <1228121096@qq.com>
2016-01-10 03:15:00 +0800

    已完成对文件头部非数据区的识别，以及数据区的读取

--- csv_plot1/Form1.Designer.cs
+++ csv_plot1/Form1.Designer.cs
@@ -28,21 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(800, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(763, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(909, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- csv_plot1/Form1.cs
+++ csv_plot1/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace csv_plot1
 {
@@ -20,6 +22,188 @@ namespace csv_plot1
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 0s
+            //string text1 = File.ReadAllText(@"C:\Users\y689\Desktop\201601SOANoise\noisemeasure\Filethru.DAT");
+            //label1.Text += text1;
+
+            //char[] splitchar = {};
+            //string[] linessplit = text1.Split(splitchar);
+
+            //label4.Text = linessplit[0];
+
+            //string text1 = File.ReadAllText(@"C:\Users\y689\Desktop\201601SOANoise\noisemeasure\Filethru.DAT");
+            //char[] splitchar = { };
+            //string[] linessplit = text1.Split(splitchar);
+            //int numoflines = linessplit.Length;
+            ////for (int ii = 0; ii < numoflines; ii++)
+            ////{
+            ////    label2.Text += linessplit[ii] + '\n';
+            ////}
+
+            char[] splitchar = { ','};
+            string a1 = "10012500000, 1.789340e-001,";
+            //string a1 = "9999502898.550724\t -134.8192138671875\t -200";
+            //string a1 = "9999502898.550724,-134.8192138671875, -200,";
+            string[] a1spl=a1.Split(splitchar);
+            int numofa1spl = a1spl.Length;
+            for (int ii = 0; ii < numofa1spl; ii++)
+            {
+                label1.Text += a1spl[ii] +'a'+ '\n';
+            }
+            label1.Text +=  'b';
+            Regex rg = new Regex(";");
+            MatchCollection mc = rg.Matches(a1);
+            label1.Text += mc.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 5s
+            string fileAdd = @"C:\Users\y689\Desktop\201601SOANoise\noisemeasure\Trace_0027.csv";
+            string alltext = File.ReadAllText(fileAdd);
+            Regex rg1 = new Regex(",");
+            Regex rg2 = new Regex(";");
+            Regex rg3 = new Regex("\t");
+
+            MatchCollection mc1 = rg1.Matches(alltext);
+            MatchCollection mc2 = rg2.Matches(alltext);
+            MatchCollection mc3 = rg3.Matches(alltext);
+            int numofcomma = mc1.Count;
+            int numofsemi = mc2.Count;
+            int numotab = mc3.Count;
+
+            int numofmainsym = numofcomma;
+            char[] splitchar = { ',' };
+
+            label4.Text = ",号" + numofcomma.ToString() + '\n';
+            label4.Text += ";号" + numofsemi.ToString() + '\n';
+            label4.Text += "Tab" + numotab.ToString() + '\n';
+
+            if (numofmainsym < numofsemi)
+            {
+                numofmainsym = numofsemi;
+                splitchar[0] = ';';
+            }                
+            if (numofmainsym < numotab)
+            {
+                numofmainsym = numotab;
+                splitchar[0] = '\t';
+            }
+
+            int numofcolumn=0;
+            string[] linesArray = File.ReadAllLines(fileAdd);
+            int numoflines = linesArray.Length;
+            string expdataforsplit = linesArray[numoflines - 2];
+            string[] expspl = expdataforsplit.Split(splitchar);
+            for(int i1=0; i1< expspl.Length; i1++)
+            {
+                if ("" != expspl[i1])
+                    numofcolumn++;
+            }
+
+            label4.Text += numoflines.ToString() + '\n';
+            label4.Text += splitchar[0].ToString() + '\n';
+            label4.Text += expdataforsplit + '\n';
+            label4.Text += numofcolumn.ToString() + '\n';
+
+            double[] col1 = new double[numoflines];
+            double[] col2 = new double[numoflines];
+            List<double> coli3 = new List<double>();
+            List<double> coli4 = new List<double>();
+            List<double> coli5 = new List<double>();
+            List<double> coli6 = new List<double>();
+            List<double> coli7 = new List<double>();
+            bool headregion = true;
+
+            double temp1 = 0;
+            bool nextline = true;
+            int headlinenum = 0;
+            for (int i2=0; i2< numoflines; i2++)
+            {
+                string[] dataspl = linesArray[i2].Split(splitchar);
+                if (headregion)
+                {
+                    if (dataspl.Length < numofcolumn)
+                        continue;
+                    for (int j2 = 0; j2 < numofcolumn; j2++)
+                    {
+                        try { temp1 = Convert.ToDouble(dataspl[j2]); }
+                        catch
+                        {
+                            nextline = true;
+                            break;
+                        }
+                        nextline = false;
+                    }
+                    if (nextline)
+                        continue;
+                    headregion = false;
+                    headlinenum = i2;
+                    i2--;
+                }
+                else
+                {
+                    for (int j2 = 0; j2 < numofcolumn; j2++)
+                    {
+                        temp1 = Convert.ToDouble(dataspl[j2]);
+                        switch(j2)
+                        {
+                            case 0:
+                                col1[i2 - headlinenum]= Convert.ToDouble(dataspl[j2]);
+                                break;
+                            case 1:
+                                col2[i2 - headlinenum] = Convert.ToDouble(dataspl[j2]);
+                                break;
+                            case 2:
+                                coli3.Add(Convert.ToDouble(dataspl[j2]));
+                                break;
+                            case 3:
+                                coli4.Add(Convert.ToDouble(dataspl[j2]));
+                                break;
+                            case 4:
+                                coli5.Add(Convert.ToDouble(dataspl[j2]));
+                                break;
+                            case 5:
+                                coli6.Add(Convert.ToDouble(dataspl[j2]));
+                                break;
+                            case 6:
+                                coli7.Add(Convert.ToDouble(dataspl[j2]));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            
+            }
+
+            label4.Text += headlinenum.ToString() + '\n';
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //// 5s
+            //StreamReader sr = new StreamReader(@"C:\Users\y689\Desktop\201601SOANoise\noisemeasure\Filethru.DAT");
+            //string str;
+            //while ((str = sr.ReadLine()) != null)
+            //{
+            //    label3.Text += str + '\n';
+            //}
+            double exp11 = 0;
+            try
+            {
+                exp11 = Convert.ToDouble("1abs");
+            }
+            catch
+            {
+                MessageBox.Show("df");
+            }
+            label4.Text += exp11.ToString() + '\n';
         }
     }
 }

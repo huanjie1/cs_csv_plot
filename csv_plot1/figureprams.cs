111eb8f37a69f4796b74a1ce70e60c5073793a81
huanjie1 <1228121096@qq.com>
2016-01-27 10:46:45 +0800

    更改了文件名

---
+++ csv_plot1/figureprams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csv_plot1
+{
+    class figureprams
+    {
+        public static int DownMargin = 30;
+        public static int LeftMargin = 30;
+        public static int RightMargin = 30;
+        public static int TopMargin = 30;
+        public static Color LineColor = Color.Blue;
+    }
+}
